0bb821465bcebdaf0881739610f5baa1d07a5ebf
hoangaka234 <85595307+hoangaka234@users.noreply.github.com>
2021-12-15 15:33:43 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `xin chào công chúa của anh`,
+    btnIntro: 'hihi',
+    title: 'Trái đất hơn 7 tỉ người mà sao chỉ có một người thươngg 😙',
+    desc: 'Anh thích em quá baby à ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100041311791353' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
